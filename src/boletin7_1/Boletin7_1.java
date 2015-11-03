b0799677e7a544b5c719a0604b1c7ed9695a408f
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2015-11-03 09:34:56 +0100

    Boletin7_1

---
+++ src/boletin7_1/Boletin7_1.java
@@ -0,0 +1,17 @@
+
+package boletin7_1;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin7_1 {
+
+    public static void main(String[] args) {
+    int num1;
+    Positivo obx = new Positivo();
+    num1 = obx.pedirNumEnteiro();
+    obx.Positivo(num1);
+    }
+    
+}
